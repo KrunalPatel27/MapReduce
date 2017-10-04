bdbaeba11f353b4ac262bba5eedf88ceea96bc8b
Krunal <krupatel27@gmail.com>
2017-10-04 18:47:28 -0500

    bufferedreader

--- Asteroid_Game_Junit_Project-master/Asteroids-Testing/Asteroids-Testing/Asteroids/mapReduce/processCoverage.java
+++ Asteroid_Game_Junit_Project-master/Asteroids-Testing/Asteroids-Testing/Asteroids/mapReduce/processCoverage.java
@@ -38,7 +38,7 @@ public class processCoverage {
                // Path pt=new Path("hdfs:/path/to/file");//Location of file in HDFS
                // FileSystem fs = FileSystem.get(conf);
                 String filename = conf.get("map.input.file");
-                BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(pt)));
+                BufferedReader br=new BufferedReader(new InputStreamReader(filename)); //.open(pt)));
                 String line;
                 line=br.readLine();
                 while (line != null){
